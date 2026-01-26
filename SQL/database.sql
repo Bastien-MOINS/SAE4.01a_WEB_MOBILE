bb22ed9c8ec47be29f6cc2c9ed1cf2c5420fbb10
Bastien-MOINS <bastien.moins@gmail.com>
2026-01-26 18:20:22 +0100

    debut database

---
+++ SQL/database.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE TABLE VOL {
+    numero_vol INT(8),
+    date_debut DATE,
+    heure_debut HOUR(HH:MM:SS),
+    date_arrivee DATE,
+    heure_arrivee HOUR(HH:MM:SS)
+}
+
+CREATE OR REPLACE TABLE AEROPORT {}
