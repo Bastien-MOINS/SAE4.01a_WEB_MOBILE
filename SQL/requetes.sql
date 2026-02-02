ffb87925ce0d6ce004a77c7e001be910ec0712c1
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-02-02 15:57:03 +0100

    realisation requete A

--- SQL/requetes.sql
+++ SQL/requetes.sql
@@ -1,1 +1,7 @@
-aaa
+-- Requetes A
+
+select ville from VOL as V from AEROPORT as A1, AEROPORT as A2
+where V.num_vol_dep = A1.numero_aeroport and V.num_vol_arr = A2.numero_aeroport
+and a1.ville = "Paris"; 
+
+-- Requetes C
